8f486d253e1027b8acaa30711d48dd97c061acf8
Pedot T <thomas.pedot@jokosun.com>
2024-02-08 14:41:27 +0100

    Add SpeedInsights component to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import Script from "next/script";
 import ClientLayout from "./ClientLayout";
@@ -29,7 +30,7 @@ export default function RootLayout({
           {children}
         </ClientLayout>
         <Analytics />
-        <SpeedInisghts />
+        <SpeedInsights />
       </body>
     </html>
   );
