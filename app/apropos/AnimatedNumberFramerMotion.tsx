f01804dd4e4ac8f0897a7933358213b4e5452358
Pedot T <thomas.pedot@jokosun.com>
2024-02-08 15:32:29 +0100

    Refactor AnimatedNumberFramerMotion component

--- app/apropos/AnimatedNumberFramerMotion.tsx
+++ app/apropos/AnimatedNumberFramerMotion.tsx
@@ -1,23 +1,16 @@
 "use client"
 
-import { useInView, useMotionValue, useSpring } from "framer-motion";
+import { useMotionValue, useSpring } from "framer-motion";
 import { useEffect, useRef } from "react";
 
 export function AnimatedNumberFramerMotion({ value }) {
     const ref = useRef(null);
     const motionValue = useMotionValue(0);
     const springValue = useSpring(motionValue, { duration: 3000 });
-    const isInView = useInView(ref, { once: true });
-    useEffect(() => {
-        if (isInView) {
-            motionValue.set(value);
-        }
-    }, [motionValue, value, isInView]);
-
     useEffect(
         () => springValue.on("change", (latest) => {
             if (ref.current && latest.toFixed(0) <= value) {
-                ref.current.textContent = latest.toFixed(0);
+                (ref.current as HTMLSpanElement).textContent = latest.toFixed(0);
             }
         }),
         [springValue, value]
