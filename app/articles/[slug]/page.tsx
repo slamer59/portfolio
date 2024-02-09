6047e4a2b5bcf25139729bfce984b4c8934af0b2
Pedot T <thomas.pedot@jokosun.com>
2024-02-09 16:54:27 +0100

    Add generateMetadata function for article page

--- app/articles/[slug]/page.tsx
+++ app/articles/[slug]/page.tsx
@@ -6,6 +6,30 @@ import { PortableText } from "@portabletext/react";
 import Image from "next/image";
 export const revalidate = 30; // revalidate at most 30 seconds
 
+export async function generateMetadata(
+    { params, searchParams },
+    parent
+) {
+    const data = await getArticleData(params.slug);
+    return {
+        title: data.title,
+        description: data.description,
+        openGraph: {
+            title: data.title,
+            description: data.description,
+            type: "website",
+            images: [
+                {
+                    url: urlFor(data.mainImage).width(800).height(600).format("webp").url(),
+                    width: 800,
+                    height: 600,
+                    alt: `Article Image ${data.title}`,
+                },
+            ],
+        },
+    };
+}
+
 export default async function BlogArticle({
     params,
 }: {
@@ -39,90 +63,3 @@ export default async function BlogArticle({
         </div>
     );
 }
-
-
-
-
-
-// const ptComponents = {
-//     types: {
-//         image: ({ value }) => {
-//             if (!value?.asset?._ref) {
-//                 return null
-//             }
-//             return (
-//                 <img
-//                     alt={value.alt || " "}
-//                     loading="lazy"
-//                     src={urlFor(value).width(320).height(240).fit("max").auto("format")}
-//                 />
-//             )
-//         }
-//     }
-// }
-
-// const article = ({ article }) => {
-//     const {
-//         title = "Missing title",
-//         name = "Missing name",
-//         categories,
-//         authorImage,
-//         body = []
-//     } = article
-//     return (
-//         <article>
-//             <h1>{title}</h1>
-//             <span>By {name}</span>
-//             {categories && (
-//                 <ul>
-//                     articleed in
-//                     {categories.map(category => <li key={category}>{category}</li>)}
-//                 </ul>
-//             )}
-//             {authorImage && (
-//                 <div>
-//                     <img
-//                         src={urlFor(authorImage)
-//                             .width(50)
-//                             .url()}
-//                         alt={`${name}"s picture`}
-//                     />
-//                 </div>
-//             )}
-//             <PortableText
-//                 value={body}
-//                 components={ptComponents}
-//             />
-//         </article>
-//     )
-// }
-
-// const query = groq`*[_type == "article" && slug.current == $slug][0]{
-//   title,
-//   "name": author->name,
-//   "categories": categories[]->title,
-//   "authorImage": author->image,
-//   body
-// }`
-// export async function getStaticPaths() {
-//     const paths = await client.fetch(
-//         groq`*[_type == "article" && defined(slug.current)][].slug.current`
-//     )
-
-//     return {
-//         paths: paths.map((slug) => ({ params: { slug } })),
-//         fallback: true,
-//     }
-// }
-
-// export async function getStaticProps(context) {
-//     // It"s important to default the slug so that it doesn"t return "undefined"
-//     const { slug = "" } = context.params
-//     const article = await client.fetch(query, { slug })
-//     return {
-//         props: {
-//             article
-//         }
-//     }
-// }
-// export default article
