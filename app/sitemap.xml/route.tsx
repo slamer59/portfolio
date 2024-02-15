cf03526dc738e55bd5379ebd5b72c55ae6998ce9
Pedot T <thomas.pedot@jokosun.com>
2024-02-15 14:33:29 +0100

    Refactor sitemap.xml/route.tsx

--- app/sitemap.xml/route.tsx
+++ app/sitemap.xml/route.tsx
@@ -1,67 +1,13 @@
 
-import { getAllPhotoProjectsSitemap, getArticlesSitemap } from "@/sanity/queries/articles";
-import { unstable_noStore as noStore } from "next/cache";
-import { domain } from "portfolio.config";
-
-export const dynamic = "force-dynamic"
-
 // eslint-disable-next-line import/prefer-default-export
 export async function GET() {
-  noStore()
-  const allPhotoProjects = await getAllPhotoProjectsSitemap()
-
-  const articles = await getArticlesSitemap();
-
-  const xmlContent = `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
-  <url> 
-    <loc>${domain}</loc>
-    <lastmod>${new Date().toISOString()}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>${domain}/apropos</loc>
-    <lastmod>${new Date().toISOString()}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>${domain}/articles</loc>
-    <lastmod>${new Date().toISOString()}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.8</priority>
-  </url>
-  ${articles.map((article) => (
-    `<url>
-      <loc>${domain}/articles/${article.currentSlug}</loc>
-      <lastmod>${new Date(article._updatedAt).toISOString()}</lastmod>
-      <changefreq>monthly</changefreq>
-      <priority>0.8</priority>
-    </url>`
-  )
-  )}
-  <url>
-    <loc>${domain}/photographie</loc>
-    <lastmod>${new Date().toISOString()}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.8</priority>
-  </url>
-  ${allPhotoProjects.map((project) => (
-    `<url>
-        <loc>${domain}/photographie/${project.currentSlug}</loc>
-        <lastmod>${new Date(project._updatedAt).toISOString()}</lastmod>
-        <changefreq>monthly</changefreq>
-        <priority>0.8</priority>    
-       </url>`
-  )
-  )}
-  <url>
-    <loc>${domain}/projets-dev</loc>
-    <lastmod>${new Date().toISOString()}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.8</priority>
-  </url>
-  </urlset>`
+  const xmlContent = `<?xml version="1.0" encoding="UTF-8"?>
+    <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      <sitemap>
+        <loc>${process.env.NEXT_PUBLIC_VERCEL_URL}/sitemap-blog.xml</loc>
+      </sitemap>
+    </sitemapindex>
+    `;
 
   return new Response(xmlContent, {
     headers: {
