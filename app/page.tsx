b4659f5bf6dc86f04df8e28cf59b7a5af7483d33
Pedot T <thomas.pedot@jokosun.com>
2024-03-08 20:40:15 +0100

    Update z-index value in ButtonsCardPrimary component

--- app/page.tsx
+++ app/page.tsx
@@ -26,8 +26,8 @@ export default function Home() {
                 style={{ backgroundImage: `url('/images/profile/717A1222.png')` }}
             >
                 <div className="container bg-light">
-                    <div className="container flex flex-row items-center gap-6 sm:flex-col md:flex-row max-w-8xl lg:px-96">
-                        <div className="w-1/3 sm:w-full md:w-full">
+                    <div className="container flex flex-row items-center gap-6 pb-32 mt-10 sm:flex-col md:flex-row max-w-8xl">
+                        <div className="">
                             <AnimatedText
                                 text="Immortaliser l'instant présent, savourer chaque moment."
                                 // text=" Un monde de photos !"
@@ -37,12 +37,12 @@ export default function Home() {
                                     Toujours en quête de nouvelles perspectives à travers l'<span className="font-bold text-primary dark:text-lime-500">objectif</span> de mon appareil photo ou les lignes de code, je m'efforce de marier l'art et la technologie pour donner vie à des <span className="font-bold text-primary dark:text-lime-500">projets uniques et inspirants</span>.
                                 </h2>
                             </AnimatedText>
-                            <div className="flex flex-row items-center justify-center gap-2 m-4 lg:flex-col xl:flex-col md:justify-start">
+                            <div className="flex flex-row items-center justify-center gap-2 m-8 lg:flex-col xl:flex-col md:justify-start">
                                 <ButtonsCardPrimary text="Lire mes articles" href="/articles" />
                                 <ButtonsCardSecondary text="Regarder mes photos" href="/photographie" />
                             </div>
                         </div>
-                        <div className="flex justify-center w-2/3 md:w-full sm:w-full md:justify-end">
+                        <div className="z-20 flex justify-center w-2/3 md:w-full sm:w-full md:justify-end">
                             <div className="flex flex-wrap items-center content-center justify-center h-48">
                                 <Image
                                     className="inline-block sm:hidden md:hidden"
@@ -69,14 +69,21 @@ export default function Home() {
                         </div>
                     </div>
                 </div>
-                <div className="relative flex flex-row w-full mx-auto bg-transparent md:grid-cols-1 h-[600px]">
+                <div className="relative flex flex-row w-full mx-auto bg-transparent md:grid-cols-1 h-[900px] md:flex-col">
                     <div className="flex flex-col items-center content-center justify-center w-full bg-opacity-50 bg-light">
                         <h2 className="flex m-4 text-4xl font-semibold text-center text-dark">
                             Ma passion de la photographie
                         </h2>
+                        <Image
+                            className="hidden md:inline-block sm:inline-block"
+                            src="/images/profile/717A1179.png"
+                            width="512"
+                            height="512"
+                            alt={"Image hero 2"}
+                        />
                     </div>
-                    <div className="flex flex-col w-full bg-[#e6d4ba]">
-                        <div className="p-2 text-2xl text-center ">
+                    <div className="flex flex-col items-center content-center justify-center w-full bg-light">
+                        <div className="p-2 m-2 text-2xl text-center">
                             <p>
                                 J'ai découvert la photographie en 2011 grâce à mon frère qui m'a donné son appareil photo reflex Canon 350D.
                             </p>
@@ -92,9 +99,20 @@ export default function Home() {
                         </div>
                     </div>
                 </div>
+                <Image
+                    className="hidden md:inline-block sm:inline-block"
+                    src="/images/profile/717A0941.png"
+                    width="512"
+                    height="512"
+                    alt={"Image hero 2"}
+                />
                 <div className="relative flex flex-row w-full mx-auto md:grid-cols-1">
-                    <div className="flex flex-col w-full h-96 bg-light">
-                        <div className="m-2 text-2xl text-center bg-light h-96">
+                    <div className="flex flex-col w-full p-4 bg-light h-[700px] ">
+                        <h2 className="flex items-center justify-center m-4 text-4xl font-semibold text-dark">
+                            Musique, Danse, Cirque
+                        </h2>
+
+                        <div className="p-4 text-2xl text-center">
                             <p>Des ami.e.s musicien.ne.s m'ont donné l'opportunité de les photographier lors de <span className="font-bold text-primary dark:text-lime-500">concerts</span>, de répétitions, de tournages de clips.</p>
                             <br />
                             <p>
@@ -110,24 +128,19 @@ export default function Home() {
                             </p>
                         </div>
                     </div>
-                    <div className="flex flex-col items-center content-center justify-center w-full bg-opacity-50 bg-light h-96">
-                        <h2 className="flex m-4 text-4xl font-semibold text-center text-dark ">
-                            Musique, Danse, Cirque
-                        </h2>
-                    </div>
+
                 </div>
-                <div className="relative flex flex-row w-full mx-auto bg-transparent md:grid-cols-1 ">
-                    <div className="flex flex-col items-center content-center justify-center w-full bg-opacity-50 bg-light h-96">
-                        <h2 className="w-full m-8 text-4xl font-semibold text-center text-dark">
-                            Interessé par mon travail ?
-                        </h2>
-                        <ButtonsCardSecondary
-                            text="Contactez moi !"
-                            href="mailto:thomas.pedot@gmail.com"
+                <div className="relative flex flex-row w-full mx-auto bg-transparent h-[450px] md:flex-col">
+                    <div className="flex flex-col items-center content-center justify-center w-full bg-light ">
+                        <Image
+                            className="hidden md:inline-block sm:inline-block"
+                            src="/images/profile/717A1217.png"
+                            width="512"
+                            height="512"
+                            alt={"Image hero 2"}
                         />
-                    </div>
-                    <div className="flex flex-col w-full h-96 bg-light ">
-                        <div className="m-2 text-2xl text-center bg-light h-96">
+                        <div className="m-2 text-2xl text-center">
+
                             <p>
                                 Vous souhaitez échanger avec moi pour un projet ?
                             </p>
@@ -140,6 +153,17 @@ export default function Home() {
                                 <span className="font-bold text-primary dark:text-lime-500">N'hésitez pas à me contacter !</span>
                             </p>
                         </div>
+
+                    </div>
+                    <div className="flex flex-col items-center content-center justify-center w-full bg-opacity-50 bg-light">
+
+                        <h2 className="w-full m-8 text-4xl font-semibold text-center text-dark">
+                            Interessé par mon travail ?
+                        </h2>
+                        <ButtonsCardSecondary
+                            text="Contactez moi !"
+                            href="mailto:thomas.pedot@gmail.com"
+                        />
                     </div>
                 </div>
             </div >
