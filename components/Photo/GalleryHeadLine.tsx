41cedda1da803601a0001b3b1238856db18b9908
Pedot T <thomas.pedot@jokosun.com>
2024-03-19 20:44:13 +0100

    Update link in GalleryHeadLine component

--- components/Photo/GalleryHeadLine.tsx
+++ components/Photo/GalleryHeadLine.tsx
@@ -20,7 +20,7 @@ export default async function GalleryHeadLine({ date, title }: { date: string, t
                                 <svg className="block w-3 h-3 mx-1 text-gray-400 rtl:rotate-180 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
                                 </svg>
-                                <a href="#" className="text-sm font-medium text-gray-700 ms-1 hover:text-dark md:ms-2 dark:text-gray-400 dark:hover:text-light ">Photographie</a>
+                                <Link href="/photographie" className="text-sm font-medium text-gray-700 ms-1 hover:text-dark md:ms-2 dark:text-gray-400 dark:hover:text-light ">Photographie</Link>
                             </div>
                         </li>
                         <li aria-current="page">
